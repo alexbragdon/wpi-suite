356a6d660b56645c9685a162bd48ccf2bc717464
Zachary Richards <zrrichards@wpi.edu>
2014-03-30 12:10:08 -0400

    zrrichards: Print out date chosen by dateChooser when save button is clicked and make global.

--- PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/view/SessionPanel.java
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/view/SessionPanel.java
@@ -47,6 +47,11 @@ public class SessionPanel extends JPanel
 	private JPanel requirementsPanel;
 
 	/**
+	 * Date chooser to select when session ends
+	 */
+	JCalendar dateChooser;
+
+	/**
 	 * Create, reset, cancel buttons at the bottom.
 	 */
 	// TODO replace JPanel with something real
@@ -93,6 +98,10 @@ public class SessionPanel extends JPanel
 		final JPanel self = this;
 		saveButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+
+				//TODO: Get add date to new session, just print it out for now to test that it works
+				System.out.println("Seleted Date: " + new SimpleDateFormat("MM-dd-yy").
+						format(dateChooser.getDate()));
 				PlanningPokerSession session = new PlanningPokerSession(0, nameField.getText(), new ArrayList<RequirementEstimate>(), sessionType.REALTIME, false, false);
 				AddPlanningPokerSessionController.getInstance().addPlanningPokerSession(session);
 				nameField.setEnabled(false);
@@ -101,25 +110,24 @@ public class SessionPanel extends JPanel
 			}
 		});
 		nameField.setPreferredSize(new Dimension (300, 30));
-		
+
 		nameField.setText(new SimpleDateFormat("MMddyy-HHmm").format(new Date()) + " Planning Poker");
-		
+
 		JPanel buttonNamePanel = new JPanel();
 		buttonNamePanel.add(nameField);
 		buttonNamePanel.add(saveButton);
-		
-		JCalendar dateChooser = new JCalendar(new Date()); //Create new JCalendar with now default selected
+
+		dateChooser = new JCalendar(new Date()); //Create new JCalendar with now default selected
 		dateChooser.setPreferredSize(new Dimension(300, 300));
 		JPanel calendarPanel = new JPanel(new BorderLayout());
 		calendarPanel.add(dateChooser, BorderLayout.CENTER);
 		calendarPanel.add(new JLabel("Choose Ending Date:"), BorderLayout.NORTH);
-		
+
 		infoPanel.add(buttonNamePanel, BorderLayout.NORTH);
 		infoPanel.add(calendarPanel, BorderLayout.CENTER);
-		
+
 		this.setLayout(new BorderLayout());
 		this.add(contentPanel, BorderLayout.CENTER); // Add scroll pane to panel
 		this.add(buttonPanel, BorderLayout.SOUTH);
 	}
-
 }
