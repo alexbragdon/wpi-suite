5415c8ee0320fb68d64e0e7660f23c6cbfec15c7
mracine <mrracine23@gmail.com>
2014-04-02 22:09:09 +0000

    Fixes EditSession button and referencing entries in the hashtable of open tabs - mrracine

--- PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/view/ViewEventController.java
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/view/ViewEventController.java
@@ -20,90 +20,87 @@ import edu.wpi.cs.wpisuitetng.modules.requirementmanager.view.requirements.Requi
  */
 
 public class ViewEventController {
-	private static ViewEventController instance = null;
-	private MainView main = null;
-	private ToolbarView toolbar = null;
-	private HashMap<PlanningPokerSession, Integer> listOfEditingSessions = new HashMap<PlanningPokerSession, Integer>();
+    private static ViewEventController instance = null;
+    private MainView main = null;
+    private ToolbarView toolbar = null;
+    private HashMap<PlanningPokerSession, Integer> listOfEditingSessions = new HashMap<PlanningPokerSession, Integer>();
 
-	/**
-	 * Default constructor for ViewEventController.  Is protected to prevent instantiation.
-	 */
-	private ViewEventController() {}
+    /**
+     * Default constructor for ViewEventController.  Is protected to prevent instantiation.
+     */
+    private ViewEventController() {}
 
-	/**
-	 * Returns the singleton instance of the vieweventcontroller.
-	
-	 * @return The instance of this controller. */
-	public static ViewEventController getInstance() {
-		if (instance == null) {
-			instance = new ViewEventController();
-		}
-		return instance;
-	}
+    /**
+     * Returns the singleton instance of the vieweventcontroller.
 
-	/**
-	 * Sets the main view to the given view.
-	
-	 * @param mainview MainView
-	 */
-	public void setMainView(MainView mainview) {
-		main = mainview;
-	}
+     * @return The instance of this controller. */
+    public static ViewEventController getInstance() {
+        if (instance == null) {
+            instance = new ViewEventController();
+        }
+        return instance;
+    }
 
-	/**
-	 * Sets the toolbarview to the given toolbar
-	 * @param tb the toolbar to be set as active.
-	 */
-	public void setToolBar(ToolbarView tb) {
-		toolbar = tb;
-		toolbar.repaint();
-	}
+    /**
+     * Sets the main view to the given view.
 
-	/**
-	 * Opens a new tab for the creation of a session.
-	 */
-	public void createSession() {
-		SessionPanel newSession = new SessionPanel();
-		main.addTab("New Session", null, newSession, "Create a new session");
-		main.invalidate(); //force the tabbedpane to redraw.
-		main.repaint();
-		main.setSelectedComponent(newSession);
-	}
-	
-	public void editSession(PlanningPokerSession session) {	    
-	    boolean exists = false;
-	    int index = 0;
-	    
-	    for(PlanningPokerSession ppSession : listOfEditingSessions.keySet())
-		{
-			if(listOfEditingSessions.containsKey(session))
-			{
-				exists = true;
-				index = listOfEditingSessions.get(session);
-				break;
-			}
-		}	
-	    
-	    if (exists == false) {
-	    	SessionPanel sessionEditor = new SessionPanel(session);
-	    	String tabName = "Edit " + session.getName();
-		    main.addTab(tabName, null, sessionEditor, "Edit this session");
-		    main.invalidate();
-		    main.repaint();
-		    main.setSelectedComponent(sessionEditor);
-		    listOfEditingSessions.put(session, main.getSelectedIndex());
-	    } else {
-	    	main.setSelectedIndex(index);
-	    }
-	}
+     * @param mainview MainView
+     */
+    public void setMainView(MainView mainview) {
+        main = mainview;
+    }
 
-	/**
-	 * Removes the tab for the given JComponent
-	 * @param comp the component to remove
-	 */
-	public void removeTab(JComponent comp)
-	{
+    /**
+     * Sets the toolbarview to the given toolbar
+     * @param tb the toolbar to be set as active.
+     */
+    public void setToolBar(ToolbarView tb) {
+        toolbar = tb;
+        toolbar.repaint();
+    }
+
+    /**
+     * Opens a new tab for the creation of a session.
+     */
+    public void createSession() {
+        SessionPanel newSession = new SessionPanel();
+        main.addTab("New Session", null, newSession, "Create a new session");
+        main.invalidate(); //force the tabbedpane to redraw.
+        main.repaint();
+        main.setSelectedComponent(newSession);
+    }
+
+    public void editSession(PlanningPokerSession session) {	    
+        boolean exists = false;
+        int index = 0;
+
+        if(listOfEditingSessions.containsKey(session))
+        {
+            exists = true;
+            index = listOfEditingSessions.get(session);
+        }
+
+        if (exists == false) {
+            SessionPanel sessionEditor = new SessionPanel(session);
+            String tabName = "Edit " + session.getName();
+            main.addTab(tabName, null, sessionEditor, "Edit this session");
+            main.invalidate();
+            main.repaint();
+            main.setSelectedComponent(sessionEditor);
+            listOfEditingSessions.put(session, main.getSelectedIndex());
+        } else {
+            index = main.indexOfTab("Edit " + session.getName());
+            main.setSelectedIndex(index);
+        }
+    }
+
+    /**
+     * Removes the tab for the given JComponent
+     * @param comp the component to remove
+     */
+    public void removeTab(JComponent comp)
+    {
         listOfEditingSessions.remove(((SessionPanel) comp).getSession());
-		main.remove(comp);
-	}
+        main.remove(comp);
+    }
 }
