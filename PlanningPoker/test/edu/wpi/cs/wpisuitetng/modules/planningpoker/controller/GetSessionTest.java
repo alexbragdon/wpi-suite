123834333cbdf4b7b113264da5878b67a2baa90f
DerekFangming <synfm@126.com>
2014-04-07 21:41:44 -0400

    Tests.Commit this because i want to sync Stephen's code
    fning

---
+++ PlanningPoker/test/edu/wpi/cs/wpisuitetng/modules/planningpoker/controller/GetSessionTest.java
@@ -0,0 +1,44 @@
+/*******************************************************************************
+ * Copyright (c) 2014 WPI-Suite
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors: Team Romulus
+ ******************************************************************************/
+package edu.wpi.cs.wpisuitetng.modules.planningpoker.controller;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+import javax.swing.table.DefaultTableModel;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import edu.wpi.cs.wpisuitetng.modules.planningpoker.MockData;
+import edu.wpi.cs.wpisuitetng.modules.planningpoker.MockNetwork;
+import edu.wpi.cs.wpisuitetng.modules.planningpoker.MockRequest;
+import edu.wpi.cs.wpisuitetng.modules.planningpoker.model.PlanningPokerSession;
+import edu.wpi.cs.wpisuitetng.modules.planningpoker.model.RequirementEstimate;
+import edu.wpi.cs.wpisuitetng.modules.planningpoker.models.characteristics.sessionType;
+import edu.wpi.cs.wpisuitetng.modules.planningpoker.view.SessionPanel;
+import edu.wpi.cs.wpisuitetng.modules.planningpoker.view.SessionRequirementPanel;
+import edu.wpi.cs.wpisuitetng.network.Network;
+import edu.wpi.cs.wpisuitetng.network.Request;
+import edu.wpi.cs.wpisuitetng.network.configuration.NetworkConfiguration;
+
+/**
+ * tests the functionality of the SessionPanel.java source file
+ * @author Team Romulus
+ *
+ *
+ */
+
+
+public class GetSessionTest {
+
+}
