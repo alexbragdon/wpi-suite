b4165668f3d7efe2beff35d1db26b631b1eb5bd0
Zachary Richards <zrrichards@wpi.edu>
2014-04-14 21:17:57 -0400

    zrrichards: Added png images and ViewDeckPanel.java

---
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/view/DeckChooser.java
@@ -0,0 +1,110 @@
+package edu.wpi.cs.wpisuitetng.modules.planningpoker.view;
+
+import java.awt.Color;
+import java.awt.GridLayout;
+import java.awt.Image;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.imageio.ImageIO;
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import edu.wpi.cs.wpisuitetng.modules.planningpoker.model.DeckSet;
+
+public class ViewDeckPanel extends JPanel {
+
+	private static final long serialVersionUID = 3047183291262652700L;
+
+	/**
+	 * The sole instance of this class
+	 */
+	private static ViewDeckPanel INSTANCE = null;
+		
+	/**
+	 * Map that maps card images to the value on the card
+	 */
+	Map<Integer, Image> cards = new HashMap<Integer, Image>();
+	
+	private ViewDeckPanel() {
+		
+		//TODO: When more decks are added, make this more robust.
+		int[] cardValues = DeckSet.getInstance().getDeck("Fibonacci"); //Assume Fibonacci deck for now
+		
+		//Load card images
+		initializeMap(cardValues);
+		
+		//set up panel and layout
+		this.setLayout(new GridLayout(2, 4));
+		JLabel[] cardLabels = getLabels(cardValues);
+		int index = 0;
+		for (JLabel label : cardLabels) {
+			if (label == null) {
+				System.out.print(index + " is null!");
+			}
+			index++;
+			label.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+			add(label);
+		}
+	}
+	
+	private JLabel[] getLabels(int[] cardValues) {
+		JLabel[] labels = new JLabel[cardValues.length];
+		for (int i = 0; i < cardValues.length; i++) {
+			System.out.println("Getting: " + i + ", " + cardValues[i]);
+			labels[i] = new JLabel(new ImageIcon(cards.get(cardValues[i])));
+		}
+		return labels;
+	}
+
+	/**
+	 * Initialize the {@code cards} Map with images of the cards
+	 */
+	private void initializeMap(int[] cardValues) {
+		
+		for (int i = 1; i < cardValues.length; i++) {
+			try {
+				cards.put(cardValues[i], loadImage(cardValues[i] + ".png"));
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+//		try {
+//			cards.put(0, loadImage("question.png"));
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+	}
+	
+	/**
+	 * Load the image with the given name
+	 * @param name The name of the image to load
+	 * @return The image
+	 * @throws IOException if the Image does not exist or cannot be loaded
+	 */
+	private Image loadImage(String name) throws IOException {
+		System.out.println("Loading: " + name);
+		return ImageIO.read(getClass().getResource(name));
+	}
+
+	/**
+	 * Gets the instance of this class. If it doesn't yet exist, it is created.
+	 * @return The sole instance of this class
+	 */
+	public static ViewDeckPanel getInstance() {
+		if (INSTANCE == null) {
+			INSTANCE = new ViewDeckPanel();
+		}
+		
+		return INSTANCE;
+	}
+	
+	public static void main(String[] args) {
+		ViewDeckPanel.getInstance();
+	}
+}
