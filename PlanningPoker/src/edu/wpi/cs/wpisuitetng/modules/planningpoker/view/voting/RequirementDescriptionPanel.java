fd55d25fef497edd930901cadad091edb5a6c364
Zachary Richards <zrrichards@wpi.edu>
2014-04-19 15:03:17 -0400

    zrrichards: Change method name

--- PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/view/voting/RequirementDescriptionPanel.java
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/view/voting/RequirementDescriptionPanel.java
@@ -44,7 +44,7 @@ public final class RequirementDescriptionPanel extends JPanel {
 	 * Update the panel's text of the requirement. Only 
 	 * needs to be used if Requirement description changes.
 	 */
-	public void update() {
+	public void updateDescription() {
 		descriptionArea.setText(this.req.getDescription());
 	}
 	
