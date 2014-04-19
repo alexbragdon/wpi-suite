e934673d41cacb1b2a8fe9a9a463be157e455cdb
Zachary Richards <zrrichards@wpi.edu>
2014-04-19 14:59:21 -0400

    zrrichards: Improve javadoc and make fields private and final

--- PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/view/voting/RequirementDescriptionPanel.java
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/view/voting/RequirementDescriptionPanel.java
@@ -10,14 +10,13 @@ import edu.wpi.cs.wpisuitetng.modules.requirementmanager.models.Requirement;
 
 /**
  * This class is a {@code JPanel} to contain the description of a {@code Requirment}.
- * 
  */
 public final class RequirementDescriptionPanel extends JPanel {
 
 	private static final long serialVersionUID = 1L;
 
-	JLabel titleLabel = new JLabel("Requirement Description:");
-	JTextArea description;
+	private final JLabel titleLabel = new JLabel("Requirement Description:");
+	private final JTextArea description;
 
 	private final Requirement req;
 
@@ -42,7 +41,8 @@ public final class RequirementDescriptionPanel extends JPanel {
 	}	
 
 	/**
-	 * Update the panel's text of the requirement
+	 * Update the panel's text of the requirement. Only 
+	 * needs to be used if Requirement description changes.
 	 */
 	public void update() {
 		description.setText(this.req.getDescription());
