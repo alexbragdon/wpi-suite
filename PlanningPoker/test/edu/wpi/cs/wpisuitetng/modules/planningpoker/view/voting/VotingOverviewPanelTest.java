cf5472ca14f8f7b45e6ed586b279129b3e47831a
XiaosongWen <xwen2@wpi.edu>
2014-04-23 02:30:59 -0400

    tests for .view.voting and some merge xwen2

---
+++ PlanningPoker/test/edu/wpi/cs/wpisuitetng/modules/planningpoker/view/voting/VotingOverviewPanelTest.java
@@ -0,0 +1,45 @@
+/*******************************************************************************
+ * Copyright (c) 2012-2014 -- WPI Suite
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package edu.wpi.cs.wpisuitetng.modules.planningpoker.view.voting;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import edu.wpi.cs.wpisuitetng.modules.planningpoker.model.PlanningPokerSession;
+import edu.wpi.cs.wpisuitetng.modules.planningpoker.model.RequirementEstimate;
+import edu.wpi.cs.wpisuitetng.modules.planningpoker.models.characteristics.SessionType;
+
+/**
+ * Description
+ *
+ * @author Team Romulus
+ * @version Apr 23, 2014
+ */
+public class VotingOverviewPanelTest {
+    private VotingOverviewPanel vop;
+    
+    @Before
+    public void setUp(){
+        RequirementEstimate testReq = new RequirementEstimate(10, "I oh so love tests", 123, false);
+        ArrayList<RequirementEstimate> listEst = new ArrayList<RequirementEstimate>();
+        listEst.add(testReq);
+        
+        PlanningPokerSession session1 = new PlanningPokerSession(6, "DummySession", "HonkHonk", new Date(), 23, 59,
+                        listEst, SessionType.REALTIME, false, false, "aGuy", "-None-");    
+        vop = new VotingOverviewPanel(listEst, 1, "admin", new VotingPanel(session1),  session1);
+    }
+    
+    @Test
+    public void test(){
+        
+    }
+}
