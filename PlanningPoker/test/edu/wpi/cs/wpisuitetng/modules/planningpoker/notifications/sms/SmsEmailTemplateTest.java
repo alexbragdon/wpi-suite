bd80591d095b53157a575758335d35ad5fc360ba
XiaosongWen <xwen2@wpi.edu>
2014-04-28 20:17:44 -0400

    add test for .notifications.sms xwen2

---
+++ PlanningPoker/test/edu/wpi/cs/wpisuitetng/modules/planningpoker/notifications/sms/SmsEmailTemplateTest.java
@@ -0,0 +1,64 @@
+/*******************************************************************************
+ * Copyright (c) 2012-2014 -- WPI Suite
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package edu.wpi.cs.wpisuitetng.modules.planningpoker.notifications.sms;
+
+import static org.junit.Assert.assertFalse;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import edu.wpi.cs.wpisuitetng.modules.core.models.User;
+import edu.wpi.cs.wpisuitetng.modules.planningpoker.model.PlanningPokerSession;
+import edu.wpi.cs.wpisuitetng.modules.planningpoker.model.RequirementEstimate;
+import edu.wpi.cs.wpisuitetng.modules.planningpoker.models.characteristics.SessionType;
+
+/**
+ * Description
+ *
+ * @author Team Romulus
+ * @version Apr 28, 2014
+ */
+public class SmsEmailTemplateTest {
+    private SmsEmailTemplate SET;
+    private User user;
+    
+    @Before
+    public void setUp(){
+        SessionClosedSmsTemplate SCST;
+        ArrayList<RequirementEstimate> requirement = new ArrayList<RequirementEstimate>();
+        requirement.add(new RequirementEstimate(0, "Test1", 0, true));
+        PlanningPokerSession session = new PlanningPokerSession(
+                        1,  "Test", "test", new Date(), 0,  0,
+                        requirement, SessionType.DISTRIBUTED, true, 
+                        true, "admin", "-None-");
+        session.setCompletionTime(new Date());
+        SCST = new SessionClosedSmsTemplate(session);
+        user = new User("Tester Test", "test", "test", 123);
+        SET = new SmsEmailTemplate(SCST);
+    }
+    
+    @Test
+    public void TestShouldReceiveNotifications(){
+        assertFalse(SET.shouldReceiveNotifications(user));
+    }
+    
+    @Test(expected=RuntimeException.class)
+    public void TestGenerateMessageException(){
+        SET.generateMessage(user);
+    }
+    
+    @Test
+    public void TestGenerateMessage(){
+        user.setCarrier("AT&T");
+        SET.generateMessage(user);
+    }
+}
---
+++ PlanningPoker/test/edu/wpi/cs/wpisuitetng/modules/planningpoker/notifications/sms/SmsSenderTest.java
@@ -0,0 +1,28 @@
+/*******************************************************************************
+ * Copyright (c) 2012-2014 -- WPI Suite
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package edu.wpi.cs.wpisuitetng.modules.planningpoker.notifications.sms;
+
+import org.junit.Before;
+
+import edu.wpi.cs.wpisuitetng.modules.planningpoker.notifications.INotificationTemplate;
+
+/**
+ * Description
+ *
+ * @author Team Romulus
+ * @version Apr 28, 2014
+ */
+public class SmsSenderTest {
+    private SmsSender SS;
+    
+    @Before
+    public void setUp(){
+      
+    }
+}
