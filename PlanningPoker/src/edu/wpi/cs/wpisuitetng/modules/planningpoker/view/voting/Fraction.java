0bc9ff4556abf913999a3da60f24928293c6399a
Zachary Richards <zrrichards@wpi.edu>
2014-04-30 00:36:02 -0400

    zrrichards: Add method to the Fraction to return true if it is compete (i.e. numerator == denominator)

--- PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/view/voting/Fraction.java
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/view/voting/Fraction.java
@@ -40,6 +40,14 @@ public class Fraction {
         return numerator;
     }
     
+    /**
+     * Returns true if the all votes have been cast
+     * @return true if the fraction's value is 1
+     */
+    public boolean isComplete() {
+    	return (numerator == denominator);
+    }
+    
     @Override
     public String toString() {
     	if (numerator == denominator) {
