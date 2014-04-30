d53d2c9f51aabc96f2f18274aeac375be53662e9
Zachary Richards <zrrichards@wpi.edu>
2014-04-30 18:32:48 -0400

    zrrichards: Add function to incrementally change between two colors given a Fraction. Code works but the initial and final colors need to be tweaked

--- PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/view/voting/ProgressBarTableCellRenderer.java
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/view/voting/ProgressBarTableCellRenderer.java
@@ -12,6 +12,8 @@ package edu.wpi.cs.wpisuitetng.modules.planningpoker.view.voting;
 import java.awt.Color;
 import java.awt.Component;
 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.JProgressBar;
 import javax.swing.JTable;
 import javax.swing.UIManager;
@@ -51,13 +53,82 @@ public class ProgressBarTableCellRenderer implements TableCellRenderer {
         	progressBar.setValue((int)(fraction.getValue() * MAX)); //else, set its progress
         }
         progressBar.setString(fraction.toString());
+        
+    	final Color INITIAL = new Color(204, 102, 102);
+    	final Color FINAL = new Color(102, 204, 102);
 
-        if (fraction.getValue() >= 0.99 && fraction.getValue() <= 1.01) {
-            progressBar.setForeground(new Color(102, 204, 102));
-        } else {
-            progressBar.setForeground(UIManager.getColor("ProgressBar.foreground"));
-        }
+        progressBar.setForeground(getColor(INITIAL, FINAL, fraction));
         
         return progressBar;
+    }
+    
+    /**
+     * Return the color associated with the given values. The color
+     * progresses form red (0%) to green (100%) in equal stages
+     * @param fraction The fraction to use to compute the color
+     * @return The shade of red/green that represents the transition
+     */
+    private static Color getColor(final Color INITIAL, final Color FINAL, Fraction fraction) {
+    	
+    	//Doing this prevents inaccuracies doing double calculations
+    	if (fraction.isComplete()) {
+    		return FINAL;
+    	}
+    	
+    	//Doing this prevents inaccuracies doing double calculations
+    	if (fraction.getNumerator() == 0) {
+    		return INITIAL;
+    	}
+    	
+    	
+    	/* Find the differences between the initial and final colors.
+    	 * "How much" the colors have to change
+    	 */
+    	double rDiff = FINAL.getRed() - INITIAL.getRed();
+    	double gDiff = FINAL.getGreen() - INITIAL.getGreen();
+    	double bDiff = FINAL.getBlue() - INITIAL.getBlue();
+    	
+    	//Find the increment to change by
+    	double rDiffIncrement = rDiff / ((double) fraction.getDenominator());
+    	double gDiffIncrement = gDiff / ((double) fraction.getDenominator());
+    	double bDiffIncrement = bDiff / ((double) fraction.getDenominator()); 
+    	
+    	//How many increments to change
+    	rDiffIncrement *= fraction.getNumerator();
+    	gDiffIncrement *= fraction.getNumerator();
+    	bDiffIncrement *= fraction.getNumerator();
+
+    	//Apply the change to the initial color to get the current color
+    	int r = (int) (INITIAL.getRed() + rDiffIncrement);
+    	int g = (int) (INITIAL.getGreen() + gDiffIncrement);
+    	int b = (int) (INITIAL.getBlue() + bDiffIncrement);
+   	
+    	return new Color(r, g, b);
+    }
+    
+    //Test method. REMOVE BEFORE MERGING
+    public static void main(String[] args) throws InterruptedException {
+    	JFrame frame = new JFrame();
+    	frame.setSize(400, 400);
+    	frame.setLocationRelativeTo(null);
+    	JPanel panel = new JPanel();
+    	
+    	
+    	frame.add(panel);
+    	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    	frame.setVisible(true);
+    	
+    	final Color INITIAL = new Color(200, 0, 0);
+    	final Color FINAL = new Color(0, 200, 0);
+    	
+    	final int total = 12; 
+    	for (int i = 0; i <= total; i++) {
+    		Fraction f = new Fraction(i, total);
+    		Color c = getColor(INITIAL, FINAL, f);
+    		System.out.println("[" + c.getRed() + ", " + c.getGreen() + ", " + c.getBlue() + "]");		
+    		panel.setBackground(c);
+    		panel.repaint();
+    		Thread.sleep(300);
+    	}
     }
 }
