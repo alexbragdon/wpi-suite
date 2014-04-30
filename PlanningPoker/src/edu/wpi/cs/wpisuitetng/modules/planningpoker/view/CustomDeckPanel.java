f623c589de1b19a5b8fdf4788080aa0f6a1c2e26
DerekFangming <synfm@126.com>
2014-04-30 17:51:34 -0400

    Change layout of custom deck panel
    fning

--- PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/view/CustomDeckPanel.java
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/view/CustomDeckPanel.java
@@ -9,6 +9,9 @@
 
 package edu.wpi.cs.wpisuitetng.modules.planningpoker.view;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -19,12 +22,13 @@ import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 import javax.swing.JTextField;
 
+import net.miginfocom.swing.MigLayout;
 import edu.wpi.cs.wpisuitetng.modules.planningpoker.model.Card;
 import edu.wpi.cs.wpisuitetng.modules.planningpoker.model.Deck;
 
 /**
  * @author Team Romulus
- * @version Iteration-1
+ * @version Iteration-6
  */
 public class CustomDeckPanel extends JPanel {
 	private Deck viewDeck;
@@ -35,7 +39,6 @@ public class CustomDeckPanel extends JPanel {
 	private Card currentCard;
 	
 	private JLabel selectionMode;
-	private JPanel selectionRadioButtons;
 	private JRadioButton singleSelect;
 	private JRadioButton multiSelect;
 	
@@ -46,32 +49,47 @@ public class CustomDeckPanel extends JPanel {
 	
 	private SessionPanel parent;
 	
+	private JLabel title;
+	
+	private JPanel custonCardPanel;
+	private JPanel deckNamePanel;
+	
 	// TODO: Need some sort of data structure to hold textboxes and card values
 	
 	public CustomDeckPanel(SessionPanel parent){
 		this.parent = parent;
 		
+		this.setLayout(new MigLayout());
 		// TODO: Create a decent layout for this panel
 		
-		this.deckName = new JLabel("Deck name");
+		this.deckName = new JLabel("Deck name    ");
 		this.deckNameTxt = new JTextField();
+		deckNameTxt.setPreferredSize(new Dimension(200, 25));
 		this.selectionMode = new JLabel("Deck selection mode");
-		this.selectionRadioButtons = new JPanel();
 		this.singleSelect = new JRadioButton("Single selection");
 		this.multiSelect = new JRadioButton("Multiple selection");
 		this.dontKnowCard = new JCheckBox("Have an \"I don't know\" card");
 		this.createDeck = new JButton("Create Deck");
 		this.cancelDeck = new JButton("Cancel Deck Creation");
+		title = new JLabel("Create a new deck");
+		title.setFont(new Font(title.getFont().getName(), Font.BOLD, 15));
+		custonCardPanel = new JPanel();
+		custonCardPanel.setPreferredSize(new Dimension(800, 400));
+		deckNamePanel = new JPanel();
+		deckNamePanel.setPreferredSize(new Dimension(320, 30));
+		deckNamePanel.setLayout(new BorderLayout());
 		
 		setupListeners();
 		
-		add(this.deckName);
-		add(this.deckNameTxt);
-		add(this.selectionMode);
-		this.selectionRadioButtons.add(this.singleSelect);
-		this.selectionRadioButtons.add(this.multiSelect);
-		add(this.selectionRadioButtons);
-		add(this.dontKnowCard);
+		add(title, "wrap");
+		deckNamePanel.add(this.deckName, BorderLayout.WEST);
+		deckNamePanel.add(this.deckNameTxt, BorderLayout.CENTER);
+		add(deckNamePanel,"span x, wrap");
+		add(custonCardPanel,"span x, wrap");
+		add(this.selectionMode, "wrap");
+		add(singleSelect,"wrap");
+		add(multiSelect,"wrap");
+		add(this.dontKnowCard,"wrap");
 		add(this.createDeck);
 		add(this.cancelDeck);
 	}
