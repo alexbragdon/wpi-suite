5535ef98608afa78c2bf45e69ce4ec10291a39e5
Zachary Richards <zrrichards@wpi.edu>
2014-04-30 00:21:11 -0400

    zrrichards: Change toString to show "Complete!" when the fraction indicates that all votes have been cast

--- PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/view/voting/Fraction.java
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/view/voting/Fraction.java
@@ -42,6 +42,10 @@ public class Fraction {
     
     @Override
     public String toString() {
+    	if (numerator == denominator) {
+    		return "Complete!";
+    	}
+    	
     	return numerator + "/" + denominator;
     }
     
