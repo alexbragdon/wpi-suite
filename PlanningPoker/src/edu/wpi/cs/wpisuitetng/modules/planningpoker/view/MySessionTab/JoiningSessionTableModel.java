1cf013580c009f4b7516a198c3dc00a44c93bc79
Zachary Richards <zrrichards@wpi.edu>
2014-04-29 21:35:27 -0400

    zrrichards: Add javadoc and refactor duplicate code into a private method

--- PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/view/MySessionTab/JoiningSessionTableModel.java
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/view/MySessionTab/JoiningSessionTableModel.java
@@ -22,44 +22,56 @@ import edu.wpi.cs.wpisuitetng.modules.planningpoker.model.RequirementEstimate;
 import edu.wpi.cs.wpisuitetng.modules.planningpoker.view.voting.Fraction;
 
 public class JoiningSessionTableModel extends AbstractTableModel {
-	
+
 	List<PlanningPokerSession> sessions;
-    final String[] COLUMNS = { "ID", "Name", "End Time", "My Progress" };
-    private final int ID_COLUMN = 0;
-    private final int NAME_COLUMN = 1;
-    private final int END_TIME_COLUMN = 2;
-    private final int MY_PROGRESS_COLUMN = 3;
-	
+	final String[] COLUMNS = { "ID", "Name", "End Time", "My Progress" };
+	private final int ID_COLUMN = 0;
+	private final int NAME_COLUMN = 1;
+	private final int END_TIME_COLUMN = 2;
+	private final int MY_PROGRESS_COLUMN = 3;
+
+	/**
+	 * Create a table model
+	 */
 	public JoiningSessionTableModel() {
 		sessions = new ArrayList<PlanningPokerSession>();
 	}
-	
+
+	/**
+	 * Create a table model given a list of sessions
+	 * @param sessions The sessions with which to populate the table
+	 */
 	public JoiningSessionTableModel(List<PlanningPokerSession> sessions) {
 		this.sessions = sessions;
 	}
-	
+
 	@Override
 	public String getColumnName(int columnIndex) {
 		if ((columnIndex < 0) || (columnIndex >= COLUMNS.length)) {
 			throw new IllegalArgumentException(columnIndex + 
 					" not between: 0 and " + COLUMNS.length);
 		}
-		
+
 		//Now we can assume the index is valid
 		return COLUMNS[columnIndex];
 	}
-	
+
+	/**
+	 * Adds the given session to the model and then repaints it
+	 * @param session The session to add
+	 */
 	public void addSession(PlanningPokerSession session) {
 		sessions.add(session);
+		fireTableChangedAll();
 	}
-	
+
 	@Override
 	public boolean isCellEditable(int rowIndex, int columnIndex) {
-		
+
 		//No cells in this table are editable
 		return false;
 	}
-	
+
 	@Override
 	public Class<?> getColumnClass(int columnIndex) {
 		switch (columnIndex) {
@@ -83,20 +95,20 @@ public class JoiningSessionTableModel extends AbstractTableModel {
 
 	@Override
 	public Object getValueAt(int row, int col) {
-		
+
 		PlanningPokerSession session = sessions.get(row);
-		
+
 		//Calculate how many of these requirements the current user has voted on
-        int myVotes = 0;
-        for (RequirementEstimate requirement : session.getRequirements()) {
-            if (requirement.getVotes().containsKey(ConfigManager.getConfig().getUserName())) {
-                myVotes++;
-            }
-        }
-        
-        //How many votes are necessary to be complete
-        int totalVotes = session.getRequirements().size();
-        
+		int myVotes = 0;
+		for (RequirementEstimate requirement : session.getRequirements()) {
+			if (requirement.getVotes().containsKey(ConfigManager.getConfig().getUserName())) {
+				myVotes++;
+			}
+		}
+
+		//How many votes are necessary to be complete
+		int totalVotes = session.getRequirements().size();
+
 		switch (col) {
 		case ID_COLUMN : return session.getID();
 		case NAME_COLUMN : return session.getName();
@@ -105,15 +117,24 @@ public class JoiningSessionTableModel extends AbstractTableModel {
 		default: throw new RuntimeException(col + " is invalid");
 		}
 	}
-	
+
 	/**
 	 * Updates the model to reflect the new list of sessions
 	 * @param newModel The new sessions with which to construct the model
 	 */
 	public void updateModel(List<PlanningPokerSession> newModel) {
-	    sessions = newModel;
-	    fireTableChanged(new TableModelEvent(this, 0, getRowCount() - 1,
-	                    TableModelEvent.ALL_COLUMNS, TableModelEvent.UPDATE));
+		sessions = newModel;
+		fireTableChangedAll();
+	}
+
+	/**
+	 * Fires a TableChanged event to all listeners for every cell in the table.
+	 * This method serves as shorthand as the same code is used many times
+	 * in this class
+	 */
+	private void fireTableChangedAll() {
+		fireTableChanged(new TableModelEvent(this, 0, getRowCount() - 1,
+				TableModelEvent.ALL_COLUMNS, TableModelEvent.UPDATE));
 	}
 
 }
